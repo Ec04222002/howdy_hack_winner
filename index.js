103c6d165646fff0876e733a12a6cad09636a22c
Elvis Chen <elvischen291@gmail.com>
2022-10-01 13:21:06 -0500

    home design

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 log = console.log
+print = console.log
 doc = document
 
 
 window.onload = () => {
-    
-    let search = doc.querySelector('#search');
+   
     let searchBox = doc.querySelector(".search-box");
-    let searchIcon = doc.querySelector(".search-icon")
     let main = doc.getElementsByTagName('main')[0]
     main.addEventListener('click', (e)=>{
         searchBox.classList.remove('border-searching')
         e.stopPropagation()
     })
+    let search = doc.querySelector('#search');
+    
     search.addEventListener('click', (e)=>{
         searchBox.classList.add('border-searching')
         e.stopPropagation()
     })
+    let goIcon = doc.querySelector('.go-icon')
+    searchBox.addEventListener('keyup', (e)=>{
+        if(e.target.value.trim().length > 0){
+            goIcon.classList.add("go-in")
+        }
+        else{
+            goIcon.classList.remove('go-in')
+        }
+    })
     
+
 
 };
 
