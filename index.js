67d8e3a65592f5a5363441a9e95463360ca01df4
tannergz123 <tanner.gao@cloudokyo.dev>
2022-10-01 22:56:49 -0500

    test API for yelp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ doc = document
 
 
 window.onload = () => {
-   
+    test();
     let searchBox = doc.querySelector(".search-box");
     let search = doc.querySelector('#search');
     let main = doc.getElementsByTagName('main')[0]
@@ -21,6 +21,12 @@ window.onload = () => {
         searchBox.classList.add('border-searching')
         e.stopPropagation()
     })
+
+    // .left-arrow-icon.addEventListener('click', (e)=>{
+    //     console.log("howdy")
+    //     e.stopPropagation()
+    // })
+    
     let goIcon = doc.querySelector('.go-icon')
     searchBox.addEventListener('keyup', (e)=>{
         if(e.target.value.trim().length > 0){
@@ -77,75 +83,19 @@ window.onload = () => {
     };
 }
 
-const apiResult = [{
-    img: "../HowdyHack2022/assets/capitol.png",
-    title: "title1"
-  }, {
-    img: "../HowdyHack2022/assets/capitol.png",
-    title: "title2"
-  }, {
-    img: "../HowdyHack2022/assets/capitol.png",
-    title: "title3"
-  },{
-    img: "../HowdyHack2022/assets/capitol.png",
-    title: "title4"
-  },{
-    img: "../HowdyHack2022/assets/capitol.png",
-    title: "title5"
-  }];
-
-const container = document.getElementById('timeline-slider');
-
-apiResult.forEach((result, idx) => {
-    // Create card element
-    const card = document.createElement('div');
-    card.classList = 'card-body draggable = "true"';
-  
-    // Construct card content
-    const content = `
-        <div class="card">
-            <div class="left-arrow-icon"><i class="fa fa-chevron-left"></i></div> 
-            <div class="right-arrow-icon"><i class="fa fa-chevron-right"></i></div> 
-            <div class="img">
-                <img src="${result.img}">
-            </div>
-            <div class="content">
-                <div class="title">
-                        ${result.title}
-                </div>
-                <div class="sub-title">
-                    Web Developer
-                </div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit modi dolorem quis quae animi nihil minus sed unde voluptas cumque.
-                </p>
-                <div class="btn">
-                    <button>Read more</button>
-                </div>
-            </div>
-        </div>
-         `;
-    container.innerHTML += content;
-});
-
-var listItems = Array.from(document.querySelectorAll(".card")); 
-function changeIndex(item, to) {
-    // Change position in array
-    arrayMove(sortables, item.index, to);
-  
-    // Change element's position in DOM. Not always necessary. Just showing how.
-    if (to === total - 1) {
-      container.appendChild(item.element);
-    } else {
-      var i = item.index > to ? to : to + 1;
-      container.insertBefore(item.element, container.children[i]);
-    }
-  
-    // Set index for each sortable
-    sortables.forEach((sortable, index) => sortable.setIndex(index));
-  }
-
+// ---------------------------API-------------------------------------
 // location score
+async function test(){
+    const response2 = await fetch('https://circumvent-cors.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=attractions&latitude=37.786882&longitude=-122.399972', {
+    headers: {
+      Authorization: "Bearer " + "jh_k1Fc1TSedLISuqFALl5AXjc21dVAoJVSwdciy-yYrVtD2NUjZpCq8pnQmU3k1JQ2C-h8pXk54bB9UvtRkp9mve1fkTL_1L3wIPwxhbsE6d-Otrz5TYYfTrQU5Y3Yx"
+    }
+}).catch(
+    err => console.log(err.response.data)
+  );
+  console.log(response2);
+}
+
 async function getPoints(latitude, longitude) {
 let response = null;
 response = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
@@ -206,6 +156,7 @@ async function getCentLatLong(city){
     log(json)
     return json['features'][0]['center'];
 }
+
 async function getLocations(latitude, longitude, radius, category) {
     let response = null;
     response = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
@@ -247,7 +198,62 @@ async function getPlaceDescription(place) {
 }
 
 
+// mapping out api result 
+
+const apiResult = [{
+    img: "../HowdyHack2022/assets/capitol.png",
+    title: "title1"
+  }, {
+    img: "../HowdyHack2022/assets/capitol.png",
+    title: "title2"
+  }, {
+    img: "../HowdyHack2022/assets/capitol.png",
+    title: "title3"
+  },{
+    img: "../HowdyHack2022/assets/capitol.png",
+    title: "title4"
+  },{
+    img: "../HowdyHack2022/assets/capitol.png",
+    title: "title5"
+  }];
+
+const container = document.getElementById('timeline-slider');
+
   // Changes an elements's position in array
   function arrayMove(array, from, to) {
     array.splice(to, 0, array.splice(from, 1)[0]);
   }
+
+apiResult.forEach((result, idx) => {
+    // Create card element
+    const card = document.createElement('div');
+    card.classList = 'card-body draggable = "true"';
+  
+    // Construct card content
+    const content = `
+        <div class="card">
+            <div class="left-arrow-icon"><i class="fa fa-chevron-left"></i></div> 
+            <div class="right-arrow-icon"><i class="fa fa-chevron-right"></i></div> 
+            <div class="img">
+                <img src="${result.img}">
+            </div>
+            <div class="content">
+                <div class="title">
+                        ${result.title}
+                </div>
+                <div class="sub-title">
+                    Web Developer
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit modi dolorem quis quae animi nihil minus sed unde voluptas cumque.
+                </p>
+                <div class="btn">
+                    <button>Read more</button>
+                </div>
+            </div>
+        </div>
+         `;
+    container.innerHTML += content;
+});
+
+var listItems = Array.from(document.querySelectorAll(".card")); 
